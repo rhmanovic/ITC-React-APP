688382e12642d52a4f2644f309012bf488bda7e3
Abdulrahman Aldhufiri <1766694-AbdulrahmanAld1@users.noreply.replit.com>
2025-01-10 23:18:36 +0000

    Your commit message

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // config.js
-export const BASE_URL = 'https://gheras.me';
+export const BASE_URL = 'https://getarabat.com';
 // export const BASE_URL = 'https://arabatcms.replit.app';
 // export const BASE_URL = 'https://0d1631b7-2ce3-431e-af36-c7e4fc0f0954-00-1sp5nem2n8wmo.sisko.replit.dev';
 export const YOUR_MERCHANT_ID = '662625076d3391f5ad60c243'; 
